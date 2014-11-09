fe0cbe33e61cb0baf42b5e9970f0c5f4be3b7d3d
laike9m <laike9m@gmail.com>
2014-11-09 21:55:20 +0800

    move peer operations out of jade

---
+++ static/js/peerOps.js
@@ -0,0 +1,55 @@
+var mode = 'send';
+//var mode = 'receive';
+window.socket = io.connect('http://localhost/', { port: 12345 });
+
+if (mode === 'send') {
+    window.socket.on('connect', function(){
+        socket.on('send', function(data){  // what is this "socket"? is it window.socket?
+            conn.send(data.data);
+            console.log('is reliable:', conn.reliable);
+            console.log("buffersize:", conn.bufferSize);
+            console.log('block ', data.index, "sent: ", Date());
+        });
+    });
+    var peer = new Peer('zuoyao', {host: '182.92.191.93', port: 9000, debug: 3});
+    peer.on('error', function(err){console.log(err)});
+    var conn = peer.connect(
+        'lizhihua',
+        {
+            reliable: true
+        }
+    );
+    conn.on('open', function(){
+        console.log("connect to peer");
+        window.socket.emit('send');
+    });
+    conn.on('error', function(err){console.log(err)});
+}
+
+if (mode === 'receive') {
+    var start = 0;
+    var peer = new Peer('lizhihua', {host: '182.92.191.93', port: 9000, debug: 3});
+    peer.on('connection', function(conn) {
+        console.log("start writing to file");
+        console.log('is reliable:', conn.reliable);
+        conn.on('data', function(data){
+            window.socket.emit('receive', {data: data, start: start});
+            start++;
+            console.log("got data", Date());
+        });
+    });
+}
+
+window.socket.on('control', function(message){
+    switch(message.type) {
+        case "disconnect":
+            peer.disconnect();
+            break;
+        case "result":
+            console.log(message.result);
+            peer.disconnect();
+            break;
+        default:
+            console.log("wrong ctrl msg: ", message);
+    }
+});
